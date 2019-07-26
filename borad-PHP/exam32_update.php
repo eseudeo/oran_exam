702e64726f726e1f5a38dd1426d24b347f687a0f
Scarlet <estherstar1114@gmail.com>
2019-07-26 15:24:56 +0900

    게시글 내용 작성 시 엔터 적용하도록
    php함수 nl2br

--- borad-PHP/exam32_update.php
+++ borad-PHP/exam32_update.php
@@ -15,32 +15,32 @@ CREATE TABLE `exam_board` (
   `editdate` datetime NOT NULL DEFAULT '1971-01-01 00:00:00' COMMENT '수정일시'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='게시판예제';
 */
-	$id = $_GET['id']; 
+$id = $_GET['id']; 
 
-	$title = $_POST['title']; 
-	$password = $_POST['password']; 
-	$author = $_POST['author']; 
-	$content = $_POST['content']; 
+$title = $_POST['title']; 
+$password = $_POST['password']; 
+$author = $_POST['author']; 
+$content = nl2br($_POST['content']); //엔터 적용
 
-    echo $title. "<br>"; 
-	echo $password. "<br>"; 
-	echo $author. "<br>"; 
-	echo $content. "<br>"; 
+echo $title. "<br>"; 
+echo $password. "<br>"; 
+echo $author. "<br>"; 
+echo $content. "<br>"; 
 
-	$insert_query = "INSERT INTO exam_board SET title='$title', password='$password', author='$author', content='$content', regdate = '".date('Y-m-d H:i:s')."' ";
+$insert_query = "INSERT INTO exam_board SET title='$title', password='$password', author='$author', content='$content', regdate = '".date('Y-m-d H:i:s')."' ";
 
-	$update_query = "UPDATE exam_board SET title='$title', password='$password', author='$author', content='$content', editdate = '".date('Y-m-d H:i:s')."' WHERE id='$id' ";
+$update_query = "UPDATE exam_board SET title='$title', password='$password', author='$author', content='$content', editdate = '".date('Y-m-d H:i:s')."' WHERE id='$id' ";
 
-//	echo $insert_query;
-//	echo $update_query;
+echo $insert_query;
+echo $update_query;
 
-	if($id == ""){
-		mysqli_query($connect_db, $insert_query);
-	}
-	if($id != ""){
-		mysqli_query($connect_db, $update_query);
-	}
+if($id == ""){
+	mysqli_query($connect_db, $insert_query);
+}
+if($id != ""){
+	mysqli_query($connect_db, $update_query);
+}
 
 //	입력이 완료되면 exam32.php 파일로 다시 돌아가도록 해주세요.(스크립트를 이용해도 됩니다)
-	echo ("<meta http-equiv='Refresh' content='1; URL=exam33.php'>");
+echo ("<meta http-equiv='Refresh' content='1; URL=exam33.php'>");
 ?>
